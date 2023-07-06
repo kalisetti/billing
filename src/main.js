36888474bf0609df47759b88848663cbc17e9e14
kalisetti <sivasankar.k2003@gmail.com>
2023-07-06 10:46:46 +0600

    add BaseCard, BaseButton & BaseDialog under UI

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import 'bootstrap/dist/js/bootstrap.js'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
 const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 app.use(router);
 app.mount('#app');
