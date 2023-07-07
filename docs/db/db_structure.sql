089ea6224f46081f0bf47aa37736b9411db0b019
kalisetti <sivasankar.k2003@gmail.com>
2023-07-07 17:04:10 +0600

    create tables customers & comments

--- docs/db/db_structure.sql
+++ docs/db/db_structure.sql
@@ -30,3 +30,42 @@ CREATE TABLE users(
 	modified_on DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+--
+-- CUSTOMERS
+--
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers(
+	name VARCHAR(140) NOT NULL PRIMARY KEY,
+	customer_name VARCHAR(140) NOT NULL UNIQUE,
+	email VARCHAR(140),
+	docstatus TINYINT(1) DEFAULT 0,
+	created_by  VARCHAR(140),
+	created_on  DATETIME DEFAULT CURRENT_TIMESTAMP,
+	modified_by VARCHAR(140),
+	modified_on DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO customers(name, customer_name) VALUES('Customer1', 'Customer1'),
+	('Customer2', 'Customer2'), ('Customer3', 'Customer3'), ('Customer4', 'Customer4'),
+	('Customer5', 'Customer5'),('Customer6', 'Customer6'),('Customer7', 'Customer7'),
+	('Customer8', 'Customer8'),('Customer9', 'Customer9'),('Customer10', 'Customer10');
+
+INSERT INTO customers(name, customer_name) VALUES('Customer-11', 'Customer-11'),
+	('Customer-12', 'Customer-12'), ('Customer-13', 'Customer-13'), ('Customer-14', 'Customer-14'),
+	('Customer-15', 'Customer-15'),('Customer-16', 'Customer-16'),('Customer-17', 'Customer-17'),
+	('Customer-18', 'Customer-18'),('Customer-19', 'Customer-19'),('Customer-20', 'Customer-20');
+
+--
+-- COMMENTS
+--
+
+CREATE TABLE comments(
+	name VARCHAR(140) NOT NULL PRIMARY KEY,
+	comment TEXT NOT NULL,
+	docstatus TINYINT(1) DEFAULT 0,
+	created_by  VARCHAR(140),
+	created_on  DATETIME DEFAULT CURRENT_TIMESTAMP,
+	modified_by VARCHAR(140),
+	modified_on DATETIME DEFAULT CURRENT_TIMESTAMP
+);
