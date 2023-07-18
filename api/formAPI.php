3671ba9fd66f0b2630cf740ec2cd1a32e01ffb49
kalisetti <sivasankar.k2003@gmail.com>
2023-07-18 23:42:04 +0600

    create SubscriptionPlan.vue

--- api/formAPI.php
+++ api/formAPI.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'App/Database/DB.php';
-require_once 'App/Utils/requestParser.php';
+require_once 'App/Utils/utils.php';
 
 use App\Database\DB;
 use App\Utils;
@@ -9,33 +9,19 @@ use App\Utils;
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 // Extract the table name and record ID from the URL parameters
-$tableName = $_GET['table'];
-$recordId = $_GET['recordId'];
+$tableName = Utils\scrub($_GET['table']);
 
 // Perform CRUD operations based on the request method
 $db = DB::getInstance();
 switch ($requestMethod) {
     case 'GET':
         // Retrieve the record from the database
-        try {
-            $rows = $db->sql("SELECT * FROM $tableName WHERE name = ?", [$recordId]);
-
-            $response = array(
-                'success' => true,
-                'error' => '',
-                'message' => '',
-                'rows' => $rows
-            );
-        } catch (Exception $e) {
-            $response = array(
-                'success' => false,
-                'error' => array('errcd' => $e->getCode(), 'errmsg' => $e->getMessage()),
-                'message' => ''
-            );
-        }
+        $payload = $_GET;
+        $response = getRecord($db, $tableName, $payload);
         break;
     case 'POST':
-        // Insert a new record into the database
+        $payload = Utils\parseRequestBody();
+        $response = saveRecord($db, $tableName, $payload);
         break;
     case 'PUT':
         // Update the existing record in the database
@@ -53,3 +39,77 @@ switch ($requestMethod) {
 header('Content-Type: application/json');
 echo json_encode($response);
 
+function getRecord($db, $tableName, $payload) {
+    try {
+        $recordId = $payload['recordId'];
+        $rows = $db->sql("SELECT * FROM $tableName WHERE name = ?", [$recordId]);
+
+        return array(
+            'success' => true,
+            'error' => '',
+            'message' => '',
+            'rows' => $rows
+        );
+    } catch (Exception $e) {
+        return array(
+            'success' => false,
+            'error' => array('errcd' => $e->getCode(), 'errmsg' => $e->getMessage()),
+            'message' => ''
+        );
+    }
+}
+
+function generateID($tableName) {
+    $id = 'name';
+    if ($tableName === 'subscription_plan') {
+        $id = 'plan_name';
+    }
+    return $id;
+}
+function constructStatement($tableName, $payload) {
+    $columns = array_keys($payload);
+    $values = array_values($payload);
+    $statement = '';
+
+    if (isset($payload['name'])) {
+        $columnPlaceholders = array_map(function($column) {
+            return "$column=?";
+        }, $columns);
+
+        $columnPlaceholdersString = implode(', ', $columnPlaceholders);
+        $statement = "UPDATE $tableName SET $columnPlaceholdersString WHERE name=?";
+        $values[] = $payload['name'];
+    } else {
+        $columnString = implode(', ', $columns);
+        $columnString = 'name, ' . $columnString;
+        $valuePlaceholders = array_fill(0, count($values), '?');
+        $valuePlaceholdersString = implode(', ', $valuePlaceholders);
+        $valuePlaceholdersString = '?, ' . $valuePlaceholdersString;
+        $statement = "INSERT INTO $tableName ($columnString) VALUES ($valuePlaceholdersString)";
+        $id = $payload['plan_name'];
+        array_unshift($values, $id);
+    }
+
+    return [$columns, $values, $statement];
+}
+function saveRecord($db, $tableName, $payload) {
+    list($columns, $values, $statement) = constructStatement($tableName, $payload);
+
+    // Insert a new record into the database
+    try {
+        $rows = $db->sql($statement, $values);
+
+        return array(
+            'success' => true,
+            'error' => '',
+            'message' => '',
+            'rows' => $rows
+        );
+    } catch (Exception $e) {
+        return array(
+            'success' => false,
+            'error' => array('errcd' => $e->getCode(), 'errmsg' => $e->getMessage()),
+            'message' => ''
+        );
+    }
+}
---
+++ src/components/form/SubscriptionPlan.vue
@@ -0,0 +1,239 @@
+<template>
+    <div class="form-page">
+        <!-- form-toolbar -->
+        <div id="form-toolbar">
+            <div id="form-indicators">
+                <div v-if="isNewRecord || hasUnsavedChanges">Not Saved</div>
+            </div>
+            <div id="form-actions">
+                <button v-if="isNewRecord || hasUnsavedChanges" class="btn btn-primary" @click="saveRecord">Save</button>
+                <button v-if="isDraft && !hasUnsavedChanges && isSubmittable" class="btn btn-primary" @click="submitRecord">Submit</button>
+                <button v-if="isSubmitted && !hasUnsavedChanges && isSubmittable" class="btn btn-primary" @click="cancelRecord">Cancel</button>
+                <button v-if="isCancelled && !hasUnsavedChanges && isSubmittable" class="btn btn-danger" @click="deleteRecord">Delete</button>
+            </div>
+        </div>
+        
+        <!-- form-section -->
+        <div id="form-body">
+            <div id="form-fields" class="row">
+                <div class="col">
+                    <div class="form-group">
+                    <label for="plan-name">Plan Name</label>
+                    <input type="text" id="plan-name" data-fieldname="plan_name" class="form-control" 
+                        v-model="recordData['plan_name']" 
+                        required
+                        >
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                    <label for="billing-interval">Billing Interval</label>
+                    <select id="billing-interval"  data-fieldname="billing_interval" class="form-select" aria-label="Billing Interval" 
+                        v-model="recordData['billing_interval']"
+                        >
+                        <option selected>Month</option>
+                        <option>Year</option>
+                    </select>
+                    </div>
+                    <div class="form-group">
+                    <label for="cost">Cost</label>
+                    <input type="number" class="form-control" data-fieldname="cost" step="0.01" 
+                        v-model="recordData['cost']"
+                        required
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- form-comments -->
+        <form-comments></form-comments>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import FormComments from '/src/components/form/FormComments.vue';
+
+export default {
+    name: "SubscriptionPlan",
+    components: {
+        'form-comments': FormComments,
+    },
+    data() {
+        return {
+            recordId: null,
+            tableName: 'subscription-plan',
+            recordData: {
+                docstatus: 0,
+            },
+            oldData: {},
+            commentInput: '',
+            comments: [],
+            newRecord: true,
+            isSubmittable: false
+        };
+    },
+    computed: {
+        tableColumns() {
+            const commonColumns = ['name', 'created_by', 'created_on', 'modified_by', 'modified_on', 'docstatus'];
+            const columns = Object.keys(this.recordData);
+            return columns.filter((column) => !commonColumns.includes(column));
+        },
+        isNewRecord() {
+            return this.newRecord;
+        },
+        hasUnsavedChanges() {
+            // Check if there are unsaved changes in the form
+            // Return true if there are unsaved changes, false otherwise
+            const newRecordKeys = Object.keys(this.recordData);
+            const oldRecordKeys = Object.keys(this.oldData);
+
+            if (newRecordKeys.length !== oldRecordKeys.length) {
+                return true;
+            } else {
+                for (let key of newRecordKeys) {
+                    if (!oldRecordKeys.includes(key)) {
+                        return true;
+                    } else {
+                        if (this.recordData[key] !== this.oldData[key]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        },
+        isDraft() {
+            return this.recordData.docstatus === 0;
+        },
+        isSubmitted() {
+            return this.recordData.docstatus === 1;
+        },
+        isCancelled() {
+            return this.recordData.docstatus === 2;
+        }
+    },
+    created() {
+        console.log('***SubscriptionPlan.created: ', this.recordData);
+        this.recordId = this.$route.params.recordId;
+        // this.tableName = this.$route.params.tableName;
+        this.fetchRecordData();
+        // this.fetchComments();
+    },
+    methods: {
+        fetchRecordData() {
+            console.log('***SubscriptionPlan.methods.fetchRecordData');
+            // Make an API call to fetch record data based on recordId and tableName
+            axios.get(`/api/formAPI.php?table=${this.tableName}&recordId=${this.recordId}`)
+                .then((response) => {
+                    const results = (response.data.rows ? response.data.rows[0] : []);
+                    if (results) {
+                        this.recordData = results;
+                        this.newRecord = false;
+                        this.oldData = { ...this.recordData };
+                    }
+                    console.log('>>>this.recordData', this.recordData);
+                })
+                .catch((error) => {
+                    console.error('Error fetching record data: ', error);
+                });
+        },
+        fetchComments() {
+            // Make an API call to fetch comments for the current record
+            axios.get(`/api/formAPI.php?table=comments&recordId=${this.recordId}`)
+                .then((response) => {
+                    this.comments = response.data.rows;
+                    console.log('this.comments',this.recordData);
+                })
+                .catch((error) => {
+                    console.error('Error fetching comments: ', error);
+                });
+        },
+        saveRecord() {
+            console.log('***SubscriptionPlan.methods.saveRecord: ', this.recordData);
+            // Make an API call to save the record data
+            axios.post(`/api/formAPI.php?table=${this.tableName}&recordId=${this.recordId}`, this.recordData)
+                .then(() => {
+                    this.fetchRecordData(); // Fetch the updated record data
+                })
+                .catch((error) => {
+                    console.error('Error saving record: ', error);
+                });
+        },
+        submitRecord() {
+            // Make an API call to submit the record
+            axios.post(`/api/submitRecord.php?table=${this.tableName}&recordId=${this.recordId}`)
+                .then(() => {
+                    this.fetchRecordData(); // Fetch the updated record data
+                })
+                .catch((error) => {
+                    console.error('Error submitting record: ', error);
+                });
+        },
+        cancelRecord() {
+            // Make an API call to cancel the submitted record
+            axios.post(`/api/cancelRecord.php?table=${this.tableName}&recordId=${this.recordId}`)
+                .then(() => {
+                    this.fetchRecordData(); // Fetch the updated record Data
+                })
+                .cancel((error) => {
+                    console.error('Error cancelling record: ', error);
+                });
+        },
+        deleteRecord() {
+            // Make an API call to delete the record
+            axios.post(`/api/deleteRecord.php?table=${this.tableName}&recordId=${this.recordId}`)
+                .then(() => {
+                    // Record deleted successfully, navigate back to ListView
+                    this.$router.push(`/desk/List/${this.tableName}`);
+                })
+                .catch((error) => {
+                    console.error('Error deleting record: ', error);
+                });
+        },
+        postComment() {
+            const commentData = {
+                comment: this.commentInput,
+                recordId: this.recordId,
+                tableName: this.tableName,
+            };
+
+            // Make an API call to post the comment
+            axios.post(`/api/formAPI.php?table=${this.tableName}`, commentData)
+                .then(() => {
+                    this.commentInput = ''; // Clear the comment input
+                    this.fetchComments(); // Fetch the updated comments        
+                })
+                .catch((error) => {
+                    console.error('Error posting comment: ', error);
+                });
+        }
+    },
+    provide() {
+        return {
+            postComment: this.postComment,
+            comments: this.comments
+        }
+    },
+    watch: {
+        recordData(newData) {
+            console.log('watch ===> recordData: ', newData);
+        }
+    }
+}
+</script>
+
+<style>
+#form-body {
+    min-height: 500px;
+}
+
+.form-group {
+    margin: 0.5rem 0;
+}
+
+.pull-right {
+    float: right;
+}
+</style>
