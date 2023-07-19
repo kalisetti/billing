12abac4daac4bc9a703badc1976ad211fb38ec89
kalisetti <sivasankar.k2003@gmail.com>
2023-07-19 20:07:14 +0600

    create makeAutoName under utils.php

--- api/App/Utils/utils.php
+++ api/App/Utils/utils.php
@@ -11,3 +11,15 @@ function scrub($input)
 {
     return str_replace([' ', '-'], '_', strtolower($input));
 }
+
+function makeAutoName($db, $series) {
+    $rows = $db->sql("SELECT * FROM series WHERE name = '$series'");
+    $currentValue = 0;
+    if (!empty($rows)) {
+        $currentValue = $rows[0]['current'] + 1;
+        $db->sql("UPDATE series SET current = $currentValue WHERE name = '$series'");
+    } else {
+        $db->sql("INSERT INTO series(name, current) VALUES('$series', 0)");
+    }
+    return $series . str_pad($currentValue,5,"0",STR_PAD_LEFT);
+}
